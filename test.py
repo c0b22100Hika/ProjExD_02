75bdb2f1ee9a0701698c99563d2c05f7b9e4cbf7
HikaruChino <c0b221001f@edu.teu.ac.jp>
2023-04-25 14:13:16 +0900

    練習１

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import pygame as pg
 import sys
-import random
 
 def main():
+    level: int = 51
+    print(level)
+
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
     clock = pg.time.Clock()
@@ -11,22 +13,30 @@ def main():
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
     tmr = 0
 
+    
+    bb_img = pg.Surface((500,500))
+    pg.draw.circle(bb_img,(255,0,0),(250,250),250)
+    bb_img.set_colorkey((0, 0, 0))
 
-    # イベントの処理
+
+    print(main.__doc__)
+
+    #イベントの処理
     while True:
-        for event in pg.event.get():    # すべてのイベントについて
-            if event.type == pg.QUIT:   # もしｘが押されたら
+        for event in pg.event.get():    #すべてのイベントについて
+            if event.type == pg.QUIT:   #もしｘが押されたら
                 return 0
 
+        """
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        """
+        
 
-        bomb_img = pg.Surface((20,20))  # ボムのイメージを作る四角を作る
-        pg.draw.circle(bomb_img, (255, 0, 0), (10,10), 10)  # 作った四角の中のどこかに円を作る
-        bomb_img.set_colorkey((0, 0, 0))    # 四角の黒い部分を透明化
+        screen.blit(bb_img, (1000,500))
 
-        screen.blit(bomb_img, [random.randint(0,1600), random.randint(0,900)])
+
 
 
 
@@ -34,7 +44,10 @@ def main():
         clock.tick(1000)
 
 
+
+
 if __name__ == "__main__":
+    
     pg.init()
     main()
     pg.quit()
