7f32dead642613cfd0f5e7bf0480e16186fb5f55
HikaruChino <c0b221001f@edu.teu.ac.jp>
2023-04-25 15:39:33 +0900

    練習5

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -6,10 +6,29 @@ import pygame as pg
 
 delta = {
         pg.K_UP:(0, -1),
-        pg.K_DOWN:(0, +1),
+        pg.K_DOWN:(0, 1),
         pg.K_LEFT:(-1, 0),
-        pg.K_RIGHT:(+1, 0),
+        pg.K_RIGHT:(1, 0),
         }
+
+
+def check_bound(scr_rct: pg.Rect, obj_rct: pg.Rect) -> tuple[bool, bool]:
+    """
+    オブジェクトが画面内or画面外かを判定　真理値タプルを返す関数
+    引数１：画面SurfaceのRect
+    引数２：こうかとんor爆弾のSurfaceのRect
+    """
+
+    yoko, tate = True, True
+
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+
+    return yoko, tate
+
+
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -26,17 +45,12 @@ def main():
     bomb_img = pg.Surface((20,20))  # ボムのイメージを作る四角を作る
     pg.draw.circle(bomb_img, (255, 0, 0), (10,10), 10)  # 作った四角の中のどこかに円を作る
     bomb_img.set_colorkey((0, 0, 0))  # 四角の黒い部分を透明化
-
     b_x = random.randint(0,1600) # ボムのｘ座標
     b_y = random.randint(0,900)  # ボムのｙ座標
-
     #screen.blit(bomb_img, [b_x, b_y])  # 爆弾の描画
-
-    vx, vy = 0, 0
-
-    bomb_rct = bomb_img.get_rect()
-    bomb_rct.center = b_x, b_y
-
+    vx, vy = 1, 1
+    bomb_rct = bomb_img.get_rect()  #爆弾の画像をそのまま使うのでなく、新しく四角を作って動かす
+    bomb_rct.center = b_x, b_y  #爆弾の中心の位置
 
     # イベントの処理
     while True:
@@ -49,21 +63,29 @@ def main():
         key_list = pg.key.get_pressed()
         for k, mv in delta.items():
             if key_list[k]:
-                kk_rct.move_ip(mv)
-
-        vx, vy = +1, +1
-
-        screen.blit(bg_img, [0, 0])  # 背景描画
-        screen.blit(kk_img, kk_rct)  # こうかとんの描画
-
-        screen.blit(bomb_img, bomb_rct)  # 爆弾の描画
-
-        bomb_rct.move_ip(vx, vy)
+                kk_rct.move_ip(mv)  #こうかとんの場所を変化させる
+        if check_bound(screen.get_rect(), kk_rct) != (True,True):
+            for k, mv in delta.items():
+                if key_list[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1])  #こうかとんの場所を変化させる
 
 
         
 
+        screen.blit(bg_img, [0, 0])  # 背景描画
+        screen.blit(kk_img, kk_rct)  # こうかとんの描画
+        
+        yoko, tate = check_bound(screen.get_rect(), bomb_rct)
 
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
+
+        bomb_rct.move_ip(vx, vy)  #爆弾の場所を変化させる
+        screen.blit(bomb_img, bomb_rct)  # 爆弾の描画
+
+        
 
         pg.display.update()
         clock.tick(1000)
